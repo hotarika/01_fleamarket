6951130823f4c67e306ff13ca4373841f0317f8f
tatata <mugicha8932@gmail.com>
2021-01-12 06:02:08 +0900

    Update: jsを最後に読み込ませるように変更

--- common/head.php
+++ common/head.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title><?= $siteTitle; ?> | FLEA MARKET</title>
    <link rel="stylesheet" href="./css/import.css">
-   <script src="./js/vender/jquery-3.5.1.min.js"></script>
-   <script src="./js/main.js"></script>
+   <script src="./js/vender/jquery-3.5.1.min.js" defer></script>
+   <script src="./js/main.js" defer></script>
    <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
 </head>
