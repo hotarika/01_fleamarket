65d12259f32d294c0da351d5edef750a6b4e2635
tatata <mugicha8932@gmail.com>
2021-01-12 02:18:58 +0900

    Update: headerにユーザーの名前を追加・軽微な修正

--- common/header.php
+++ common/header.php
@@ -1,3 +1,11 @@
+<?php
+if (!empty($_SESSION)) {
+   $headerSession = getUser($_SESSION['user_id']);
+   $headerUsername = $headerSession['username'] ?? '名前未設定';
+}
+
+?>
+
 <header id="js-a-hdr" class="l-hdr">
    <div class="p-hdr__inner">
       <h1 class="p-hdr__logo"><a href="index.php">FLEA MARKET</a></h1>
@@ -6,8 +14,11 @@
 
             <?php if (!empty($_SESSION['user_id'])) {
             ?>
-               <li class="p-hdr-nav__item"><a href="f-logout.php">ログアウト</a></li>
+               <li class="p-hdr-nav__item" style="color:white;font-size:15px">
+                  <?= $headerUsername  ?>
+               </li>
                <li class="p-hdr-nav__item"><a href="a-mypage.php">マイページ</a></li>
+               <li class="p-hdr-nav__item"><a href="f-logout.php">ログアウト</a></li>
             <?php } else {
             ?>
                <li class="p-hdr-nav__item"><a href="b-login.php">ログイン</a></li>
